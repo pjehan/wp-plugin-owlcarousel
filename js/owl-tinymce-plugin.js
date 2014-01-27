72b5b01354ae919c042a6302cb1128552e52898d
pjehan <pierre.jehan@gmail.com>
2014-01-27 23:04:38 +0100

    Update the shortcode with default options (category, singleItem and autoPlay

--- js/owl-tinymce-plugin.js
+++ js/owl-tinymce-plugin.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         init: function(ed, url) {
             ed.addCommand('owl_insert_shortcode', function() {
                 //selected = tinyMCE.activeEditor.selection.getContent();
-                content = '[owl-carousel]';
+                content = '[owl-carousel category="Uncategorized" singleItem="true" autoPlay="true"]';
                 tinymce.execCommand('mceInsertContent', false, content);
             });
             ed.addButton('owl_button', {title: 'Insert shortcode', cmd: 'owl_insert_shortcode', image: url + '/../images/owl-logo-16.png'});
