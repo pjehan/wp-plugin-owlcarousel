4d5078082994059c4191dd055e1f34fe2314c73a
Rasmus Taarnby <rt@peytz.dk>
2015-06-03 11:04:36 +0200

    Updated: Plugin version number

--- owl-carousel.php
+++ owl-carousel.php
@@ -4,7 +4,7 @@ Plugin Name: Owl Carousel
 Description: A simple plugin to include an Owl Carousel in any post
 Author: Pierre Jehan
 Contributer: Rasmus Taarnby
-Version: 1.0.1
+Version: 1.0.2
 Text Domain: owl-carousel
 Domain Path: /languages
 Author URI: http://www.pierre-jehan.com
