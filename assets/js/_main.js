ddd43025166b4c2fdd30f026603ca15956563490
Kristoffe Biglete <krb@peytz.dk>
2015-10-13 09:16:46 +0000

    Formatting

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,56 +1,56 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function( $ ) {
 
     var carouselIds = new Array();
 
-    $(".owl-carousel-plugin").each(function() {
-        carouselIds.push($(this).attr("id"));
-    });
+    $( '.owl-carousel-plugin' ).each( function() {
+        carouselIds.push( $( this ).attr( 'id' ) );
+    } );
 
-    for (var i in carouselIds) {
+    for ( var i in carouselIds ) {
         var params = {};
-        var datas = $(document.getElementById(carouselIds[i])).data();
-        for (var paramName in datas) {
-            var data = $(document.getElementById(carouselIds[i])).data(paramName);
-            if (data !== "") {
+        var datas = $( document.getElementById( carouselIds[i] ) ).data();
+        for ( var paramName in datas ) {
+            var data = $( document.getElementById( carouselIds[i] ) ).data( paramName );
+            if ( data !== '' ) {
                 // If it's an array (contains comma) parse the string to array
-                if(String(data).indexOf(",") > -1) {
-                    data = data.split(",");
+                if( String( data ).indexOf( ',' ) > -1 ) {
+                    data = data.split( ',');
                 }
 
                 // New random param not available in Owl Carousel
-                if(paramName == "random") {
-                    params[owlCarouselParamName("beforeInit")] = function(elem) {
-                        random(elem);
+                if( paramName == 'random' ) {
+                    params[ owlCarouselParamName( 'beforeInit' ) ] = function( elem ) {
+                        random( elem );
                     };
                 } else {
-                    params[owlCarouselParamName(paramName)] = data;
+                    params[ owlCarouselParamName( paramName ) ] = data;
                 }
             }
         }
 
-        $(document.getElementById(carouselIds[i])).owlCarousel(params);
+        $( document.getElementById( carouselIds[i] ) ).owlCarousel( params );
     }
 
     /**
      * Sort random function
      * @param {Selector} owlSelector Owl Carousel selector
      */
-    function random(owlSelector){
-        owlSelector.children().sort(function(){
-            return Math.round(Math.random()) - 0.5;
-        }).each(function(){
-            $(this).appendTo(owlSelector);
-        });
+    function random( owlSelector ){
+        owlSelector.children().sort( function() {
+            return Math.round( Math.random() ) - 0.5;
+        } ).each( function() {
+            $( this ).appendTo( owlSelector );
+        } );
     }
 
-});
+} );
 
 /**
  * Fix Owl Carousel parameter name case.
  * @param {String} paramName Parameter name
  * @returns {String} Fixed parameter name
  */
-function owlCarouselParamName(paramName) {
+function owlCarouselParamName( paramName ) {
 
     var parameterArray = {
         ANIMATEIN: "animateIn",
@@ -103,5 +103,5 @@ function owlCarouselParamName(paramName) {
         VIDEOWIDTH: "videoWidth",
     };
 
-    return parameterArray[paramName.toUpperCase()];
+    return parameterArray[ paramName.toUpperCase() ];
 }
