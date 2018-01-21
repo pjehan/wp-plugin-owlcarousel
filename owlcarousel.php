dc458540ba507558d7a25f8b67c3af3db4885dc3
gordielachance <gordie.lachance@gmail.com>
2018-01-21 19:56:50 +0100

    moved widget to its own file
    + renamed owlc() to wp_owlc()
    + renamed OwlCarousel to WP_OwlCarousel

--- owlcarousel.php
+++ owlcarousel.php
@@ -8,7 +8,7 @@
   Licence: GPL2
  */
 
-class OwlCarousel {
+class WP_OwlCarousel {
     /** Version ***************************************************************/
     /**
     * @public string plugin version
@@ -41,7 +41,7 @@ class OwlCarousel {
     public static function instance() {
         
             if ( ! isset( self::$instance ) ) {
-                    self::$instance = new OwlCarousel;
+                    self::$instance = new WP_OwlCarousel;
                     self::$instance->setup_globals();
                     self::$instance->includes();
                     self::$instance->setup_actions();
@@ -71,7 +71,9 @@ class OwlCarousel {
     }
     
     function includes(){
+        require_once($this->plugin_dir . 'wp-owlc-widgets.php');
     }
+    
     function setup_actions(){
         add_theme_support('post-thumbnails');
         
@@ -283,7 +285,7 @@ class OwlCarousel {
      */
 
     function widget_init() {
-        register_widget("owl_Widget");
+        register_widget("WP_OwlCarousel_Widget");
     }
     
     /**
@@ -505,57 +507,8 @@ class OwlCarousel {
     
 }
 
-class owl_Widget extends WP_Widget {
-
-    public function __construct() {
-        parent::__construct('owl_Widget', 'Owl Carousel', array('description' => __('A Owl Carousel Widget', 'text_domain')));
-    }
-
-    public function form($instance) {
-        if (isset($instance['title'])) {
-            $title = $instance['title'];
-        } else {
-            $title = __('Widget Carousel', 'text_domain');
-        }
-        if (isset($instance['category'])) {
-            $carousel = $instance['category'];
-        } else {
-            $carousel = 'Uncategorized';
-        }
-        ?>
-        <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-        </p>
-        <p>
-            <label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Carousel:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('category'); ?>" name="<?php echo $this->get_field_name('category'); ?>" type="text" value="<?php echo esc_attr($carousel); ?>" />
-        </p>
-        <?php
-    }
-
-    public function update($new_instance, $old_instance) {
-        $instance = array();
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['category'] = strip_tags($new_instance['category']);
-
-        return $instance;
-    }
-
-    public function widget($args, $instance) {
-        extract($args);
-        $title = apply_filters('widget_title', $instance['title']);
-        echo $before_widget;
-        if (!empty($title))
-            echo $before_title . $title . $after_title;
-        echo owl_function(array(category => $instance['category'], singleItem => "true", autoPlay => "true", pagination => "false"));
-        echo $after_widget;
-    }
-
+function wp_owlc() {
+	return WP_OwlCarousel::instance();
 }
 
-function owlc() {
-	return OwlCarousel::instance();
-}
-
-owlc();
+wp_owlc();
